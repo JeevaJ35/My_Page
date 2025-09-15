70dd19701f82e3f1e46cfde3621b71ec6d6ef91e
Jeevanandham R <jeevarajasekarj35@gmail.com>
2025-09-15 10:35:21 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+   base: "/My_Page/",  // 👈 use your repo name
   plugins: [react()],
   server: { port: 5173 },
   preview: { port: 5173 }
